d41cb5669f7e91d10c9f8b9b181553cedc38f092
Bakyt <kingpro6561@gmail.com>
2023-05-12 21:29:54 +0600

    update homework

--- src/components/hooks/timer.js
+++ src/components/hooks/timer.js
@@ -37,6 +37,7 @@ export const useTimer = (toggleStartHandler) => {
         setActivateInterval(true);
     };
     const restartTimer = () => {
+        toggleStartHandler()
         stopTimer();
         setTimer(0);
     };
